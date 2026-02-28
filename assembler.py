154ca1bac16c9a6433fb2ab13cde3d2c2f71d8ee
jrandlejr <randleja@hendrix.edu>
2026-02-28 12:54:06 -0600

    created JUMP_TABLE

---
+++ assembler.py
@@ -0,0 +1,27 @@
+# Nand2Tetris Project 6: Hack Assembler (Python)
+# Author: James
+
+import sys
+# Pulled Translation tables from Nand2Tetris Chapter 6
+DEST_TABLE = {
+    None: "000",
+    "M": "001",
+    "D": "010",
+    "MD": "011",
+    "A":  "100",
+    "AM": "101",
+    "AD": "110",
+    "AD": "110",
+    "AMD": "111"
+}
+
+JUMP_TABLE = {
+    None:"000",
+    "JGT": "001",
+    "JEQ": "010",
+    "JGE": "011",
+    "JLT": "100",
+    "JNE": "101",
+    "JLE": "110",
+    "JMP": "111"
+}
